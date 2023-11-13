e808cfbbe9b1f6819de3a581f26e26ed3b6df5d9
oyekamal <autocratickamal1@gmail.com>
2023-11-13 21:34:12 +0500

    view for city updated

--- app/accountProfile/views.py
+++ app/accountProfile/views.py
@@ -1,12 +1,17 @@
 from dj_rest_auth.registration.views import RegisterView
 from .serializers import CustomRegisterSerializer, LanguageSerializer, UserLanguageUpdateSerializer, UserSerializer, CustomUserUpdateSerializer
-from .models import Language, CustomUser
+from .models import Language, CustomUser, Country, City
 from rest_framework import generics, status
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
+from .serializers import CountrySerializer, CitySerializer
+from rest_framework import filters
+from django_filters import rest_framework as drf_filters
+from .serializers import CustomUserSerializer
+
 
 class CustomRegisterView(RegisterView):
     serializer_class = CustomRegisterSerializer
@@ -21,6 +26,7 @@ class CustomRegisterView(RegisterView):
     def perform_create(self, serializer):
         user = serializer.save(self.request)
         # Additional logic if needed
+
 
 class LanguageViewSet(ModelViewSet):
     queryset = Language.objects.all()
@@ -38,20 +44,18 @@ class UserLanguageUpdate(generics.RetrieveUpdateAPIView):
 
     def patch(self, request, *args, **kwargs):
         instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer = self.get_serializer(
+            instance, data=request.data, partial=True)
         serializer.is_valid(raise_exception=True)
         self.perform_update(serializer)
         updated_instance = self.get_object()  # Get the updated user instance
         return Response(UserSerializer(updated_instance).data, status=status.HTTP_200_OK)
 
 
-from rest_framework import generics
-from .models import CustomUser
-from .serializers import CustomUserSerializer
-
 class CustomUserDetail(generics.RetrieveAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
+
 
 class CustomUserUpdate(generics.UpdateAPIView):
     queryset = CustomUser.objects.all()
@@ -61,30 +65,15 @@ class CustomUserUpdate(generics.UpdateAPIView):
 
     def get_object(self):
         return self.request.user
-    
-    
-from .models import Country, City
-from .serializers import CountrySerializer, CitySerializer
+
 
 class CountryViewSet(ModelViewSet):
     queryset = Country.objects.all()
     serializer_class = CountrySerializer
 
+
 class CityViewSet(ModelViewSet):
     queryset = City.objects.all()
     serializer_class = CitySerializer
-
-    # def get_queryset(self):
-    #     """
-    #     Optionally restricts the returned cities to a given country,
-    #     by filtering against a `country` query parameter in the URL.
-    #     """
-    #     queryset = City.objects.all()
-    #     country = self.request.query_params.get('country', None)
-    #     if country is not None:
-    #         country_objs = Country.objects.filter(name__name=country)
-    #         if country_objs:
-    #             queryset = City.objects.filter(country=country_objs.first())
-    #         else:
-    #             return []
-    #     return queryset
+    filter_backends = [filters.SearchFilter, drf_filters.DjangoFilterBackend]
+    filterset_fields = ['name', 'country']
