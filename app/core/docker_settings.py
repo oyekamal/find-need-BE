266326d911d0907843dc430c622557340b6af164
oyekamal <autocratickamal1@gmail.com>
2023-11-13 22:41:01 +0500

    CORS_ORIGIN_ALLOW_ALL

--- app/core/docker_settings.py
+++ app/core/docker_settings.py
@@ -30,13 +30,11 @@ print("--"*30)
 print(ALLOWED_HOSTS)
 print("--"*30)
 
-CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOW_ALL_ORIGINS = True
 SECURE_CROSS_ORIGIN_OPENER_POLICY = None
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_ALLOW_ALL = True
+
 allowed_hosts = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 CORS_ALLOWED_ORIGINS = ['https://' + host for host in allowed_hosts] + ['http://' + host for host in allowed_hosts]
