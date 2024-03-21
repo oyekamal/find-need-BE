204796e17b7df4ac0225482239bdf95f8e923d02
oyekamal <autocratickamal1@gmail.com>
2024-03-21 11:37:38 +0500

    env updated

--- app/core/settings.py
+++ app/core/settings.py
@@ -1,6 +1,11 @@
-local = False
+import os
 
-if local:
+local = os.environ.get("LOCAL", "True")
+
+
+if local == "True":
+    print("local settings...!")
     from .local_settings import *
 else:
+    print("docker settings...!")
     from .docker_settings import *
